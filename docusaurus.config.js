e5b7dd36a6d70404f0449a62249577e8d1cfabdc
Max Bronsema <bronsem@wwu.edu>
2023-05-23 14:02:45 -0700

    Max can't spell docusaurus properly.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'WWU-MaxBronsema', // Usually your GitHub org/user name.
-  projectName: 'archipelago-documentation-docsaurus', // Usually your repo name.
+  projectName: 'archipelago-documentation-docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
